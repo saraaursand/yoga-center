2b78d639f2cfa10215783b0f92ebc222b3b536a8
nabrekke <nabrekke@stud.ntnu.no>
2025-05-07 13:53:07 +0200

    Updated app.vue to include footer and layout

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
+  <div class="page-wrapper">
     <NuxtRouteAnnouncer />
     <NuxtPage />
+    <Footer />
   </div>
 </template>
+
+<script setup>
+import Footer from '~/components/Footer.vue'
+</script>
+
+<style>
+.page {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
