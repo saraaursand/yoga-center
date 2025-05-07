0c46354c684a8e18ab66a916f1a96b8440c54002
nabrekke <nabrekke@stud.ntnu.no>
2025-05-07 13:47:28 +0200

    Created footer

---
+++ components/footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer>
+    <div>
+      <img src="/logo.png" alt="Yoga logo" />
+    </div>
+    <div class="address">
+      <p><strong>Address:</strong></p>
+      <p>Via Andrea Maria Ampère, 2</p>
+      <p>20133 Milano MI</p>
+      <p>Italy</p>
+    </div>
+  </footer>
+</template>
+
