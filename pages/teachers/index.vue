46e7c7bbf443d80cc0e3812ea41c2a316fd45574
Siri Stølevik <siri.stolevik@gmail.com>
2025-05-31 18:14:12 +0200

    Add link for each teacher

--- pages/teachers/index.vue
+++ pages/teachers/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <h1 >Our Teachers</h1>
+    <h1>OUR TEACHERS</h1>
     <div v-for="(teacher, idx) in teachers" :key="teacher.id">
-      <IntroCard
-        :imgSrc="teacher.pic"
-        :header="teacher.name"
-        :description="teacher.intro"
-        :imageRight="idx % 2 === 1"
-      />
+      <NuxtLink :to="`/teachers/${teacher.name}`" style="text-decoration: none">
+        <IntroCard
+          :imgSrc="teacher.pic"
+          :header="teacher.name"
+          :description="teacher.intro"
+          :imageRight="idx % 2 === 1"
+        />
+      </NuxtLink>
     </div>
   </div>
 </template>
@@ -31,3 +33,10 @@ onMounted(() => {
   getTeachers();
 });
 </script>
+
+<style scoped>
+a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
