e5028bada228b25a76514a6546455da19457f7c6
Siri Stølevik <siri.stolevik@gmail.com>
2025-05-31 18:05:12 +0200

    Use IntroCard component

--- pages/teachers/index.vue
+++ pages/teachers/index.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <h1>Our Teachers</h1>
+    <h1 >Our Teachers</h1>
+    <div v-for="(teacher, idx) in teachers" :key="teacher.id">
+      <IntroCard
+        :imgSrc="teacher.pic"
+        :header="teacher.name"
+        :description="teacher.intro"
+        :imageRight="idx % 2 === 1"
+      />
+    </div>
   </div>
-  <ul>
-    <li v-for="teacher in teachers" :key="teacher.id">{{ teacher.name }}</li>
-  </ul>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
+import IntroCard from "~/components/IntroCard.vue";
 
 const { $supabase } = useNuxtApp();
 const teachers = ref([]);
