74a52d7ec0a5a1850149560e67f663b5e01d0122
Siri Stølevik <siri.stolevik@gmail.com>
2025-06-06 21:09:23 +0200

    display seminars on seminars page

--- pages/activities/seminars/index.vue
+++ pages/activities/seminars/index.vue
@@ -1,5 +1,48 @@
 <template>
-    <div>
-      <h1>Our Seminars</h1>
+  <div>
+    <h1>OUR SEMINARS</h1>
+    <div v-for="(seminar, idx) in seminars" :key="seminar.id">
+      <NuxtLink
+        :to="`/activities/classes/${seminar.name}`"
+        style="text-decoration: none"
+      >
+        <IntroCard
+          :imgSrc="seminar.pic"
+          :header="seminar.name"
+          :description="seminar.intro"
+          :imageRight="idx % 2 === 1"
+        />
+      </NuxtLink>
     </div>
-  </template>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import IntroCard from "~/components/IntroCard.vue";
+
+const { $supabase } = useNuxtApp();
+const seminars = ref([]);
+
+async function getClasses() {
+  const { data, error } = await $supabase
+    .from("activities")
+    .select("*")
+    .eq("type", "seminar");
+  if (error) {
+    console.error("Supabase error:", error);
+  }
+  seminars.value = data || [];
+}
+
+onMounted(() => {
+  getClasses();
+});
+</script>
+
+<style scoped>
+a {
+  color: inherit;
+  text-decoration: none;
+}
+</style>
