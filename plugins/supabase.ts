a2aedfc4eb5673c08fa14907cac08d9aab20d3c8
Siri Stølevik <siri.stolevik@gmail.com>
2025-06-08 20:42:25 +0200

    remove log

--- plugins/supabase.ts
+++ plugins/supabase.ts
@@ -2,10 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig();
-
-  console.log("Supabase URL:", config.public.supabaseUrl);
-  console.log("Supabase Key:", config.public.supabaseAnonKey);
-
   const supabase = createClient(
     config.public.supabaseUrl,
     config.public.supabaseAnonKey
