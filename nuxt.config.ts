234633ebda3d66d889dc6dc3e530f7829b9db997
Siri Stølevik <siri.stolevik@gmail.com>
2025-06-13 11:15:55 +0200

    specify rendering mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
   runtimeConfig: {
     public: {
       supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
